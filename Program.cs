b47a2585e6a0169ffc4df812b513060d4bf21649
Ngala Talla <ngala.talla@mdh.se>
2020-06-16 15:40:24 +0200

    Cleaned the program

--- Program.cs
+++ Program.cs
@@ -1,24 +1,25 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using RestSharp;
 using RestSharp.Authenticators;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GetGitHubRepos
 {
   class Program
-  {
+  { 
+    //Enter your Github Login Vairables
+    private const string Username = "";
+    private const string Password = "";
+    private const string Resource = "orgs/<your Org>/repos?page=1&per_page=200";
+
     static void Main(string[] args)
     {
-      var client = new RestClient("https://api.github.com");
-      client.Authenticator = new HttpBasicAuthenticator("ngalakvist", "r7dyAaHyYG38Jmt");
+      var client = new RestClient("https://api.github.com");    
+      client.Authenticator = new HttpBasicAuthenticator(Username, Password);
      
-      var request = new RestRequest("orgs/mdh-se/repos?page=1&per_page=200", DataFormat.Json);
+      var request = new RestRequest(Resource, DataFormat.Json);
       var response = client.Get(request);
       var jsonListRepos =  response.Content;
       var jsonArray = JToken.Parse(jsonListRepos);
@@ -29,7 +30,7 @@ namespace GetGitHubRepos
         repo.Add(name.ToString());
       }   
       repo.ToList().ForEach(m => Console.WriteLine(m));
-      Console.WriteLine("Antal :"+ repo.Count);
+      Console.WriteLine("Number of Repos :"+ repo.Count);
       Console.ReadLine();
     }
   }
